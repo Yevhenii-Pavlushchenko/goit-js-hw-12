c78eca4fbc28b1748b1d9b6ea1b28d2b634f9f54
JustFun <paw93@ukr.net>
2026-02-18 11:19:54 +0200

    change clases

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,7 +5,7 @@ const API_KEY = '54685682-80977b0b5ca1319a1902ced87';
 export const perPage = 15;
 axios.defaults.baseURL = BASE_URL;
 
-export async function request(query, page) {
+export async function getImagesByQuery(query, page) {
   const response = await axios({
     url: BASE_URL,
     method: 'get',
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { perPage, request } from './js/pixabay-api';
+import { perPage, getImagesByQuery } from './js/pixabay-api';
 import {
   createGallery,
   hideLoader,
@@ -6,17 +6,18 @@ import {
   loadBtn,
   showLoader,
   showLoadMoreButton,
+  clearGallery,
 } from './js/render-functions';
-import { clearGallery } from './js/render-functions';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
-const jsImgTotal = document.querySelector('.img-total');
+const outputTotal = document.querySelector('.output-total');
 
 form.addEventListener('submit', onSubmitForm);
 loadBtn.addEventListener('click', onLoadMoreClick);
-let resInput = '';
+let inputValue = '';
 let pageNum = 1;
 let totalPages = 0;
 let imagesCount = 0;
@@ -30,16 +31,16 @@ async function onSubmitForm(event) {
   hideLoadMoreButton();
   clearGallery();
 
-  resInput = form.elements['search-text'].value.trim();
+  inputValue = form.elements['search-text'].value.trim();
   try {
-    if (resInput === '') {
+    if (inputValue === '') {
       iziToast.warning({
         title: 'warning',
         position: 'topRight',
         message: 'Please enter a search query!',
       });
 
-      jsImgTotal.textContent = ``;
+      outputTotal.textContent = ``;
 
       hideLoadMoreButton();
       hideLoader();
@@ -48,14 +49,14 @@ async function onSubmitForm(event) {
     }
 
     showLoader();
-    const { hits, totalHits } = await request(resInput, pageNum);
+    const { hits, totalHits } = await getImagesByQuery(inputValue, pageNum);
     totalPages = Math.ceil(totalHits / perPage);
 
     imagesCount += hits.length;
     if (imagesCount > totalHits) {
       imagesCount = totalHits;
     }
-    jsImgTotal.textContent = `Total images: ${imagesCount}/${totalHits}`;
+    outputTotal.textContent = `Total images: ${imagesCount}/${totalHits}`;
 
     if (!hits || hits.length === 0) {
       console.log(hits);
@@ -65,7 +66,6 @@ async function onSubmitForm(event) {
         backgroundColor: `#4e75ff`,
         messageColor: `#ffffff`,
         position: `topRight`,
-        maxWidth: `432px`,
       });
       hideLoader();
       return;
@@ -77,13 +77,6 @@ async function onSubmitForm(event) {
   } catch (error) {
     hideLoadMoreButton();
     console.log(error.message);
-    // iziToast.show({
-    //   message: 'Please enter a search query!',
-    //   backgroundColor: `#4e75ff`,
-    //   messageColor: `#ffffff`,
-    //   position: `topRight`,
-    //   maxWidth: `432px`,
-    // });
   } finally {
     form.elements['search-text'].value = '';
     hideLoader();
@@ -94,13 +87,13 @@ async function onLoadMoreClick() {
   showLoader();
   try {
     pageNum += 1;
-    const { hits, totalHits } = await request(resInput, pageNum);
+    const { hits, totalHits } = await getImagesByQuery(inputValue, pageNum);
 
     imagesCount += hits.length;
     if (imagesCount > totalHits) {
       imagesCount = totalHits;
     }
-    jsImgTotal.textContent = `Total images: ${imagesCount}/${totalHits}`;
+    outputTotal.textContent = `Total images: ${imagesCount}/${totalHits}`;
     if (pageNum < totalPages) {
       showLoadMoreButton();
     } else {
@@ -109,7 +102,6 @@ async function onLoadMoreClick() {
         backgroundColor: `#4e75ff`,
         messageColor: `#ffffff`,
         position: `topRight`,
-        maxWidth: `432px`,
       });
       hideLoadMoreButton();
     }
